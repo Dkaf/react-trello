ad7faaa29e35720acac1b9ab0234bc6e7984569c
daniel kafer <daniel.j.kafer@gmail.com>
2016-11-16 15:44:39 -0600

    made list stateful component and added input form

--- js/components/list.js
+++ js/components/list.js
@@ -2,17 +2,38 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Card = require('./card');
 
-var List = function(props) {
-	return (
-		<div className="list">
-			<div className="list_title">
-				{props.title}
-			</div>
-			<div className="list_cards">
-				{props.cards}
-			</div>
-		</div>
-	)
+
+var Input = function(props) {
+	return <input type={props.type} placeholder={props.placeholder} onChange={props.onChange} />
 };
+
+
+var List = React.createClass({
+
+	onAddInputChanged: function() {
+		console.log('input changed');
+	},
+
+	onAddSubmit: function(e) {
+		e.preventDefault();
+		console.log('form submitted');
+	},
+
+	render: function(props) {
+		return (
+			<div className="list">
+				<div className="list_title">
+					{this.props.title}
+				</div>
+				<div className="list_cards">
+					{this.props.cards}
+				</div>
+				<form onSubmit={this.onAddSubmit}>
+					<Input type="text" placeholder="New card" onChange={this.onAddInputChanged}/>
+				</form>
+			</div>
+		);
+	}
+});
 
 module.exports = List;
